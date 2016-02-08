7d013bc98aaef1de09be1a6e7fdc65c1fb315a96
DataNick <DataNick@users.noreply.github.com>
2016-02-08 15:57:49 -0500

    Fixed loop when searching for slug on root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-
     resources :pages #/admin/pages
-
   end
 
   # Retrieve slugs of pages and make them navigable routes
   # Checking all pages that don't have a slug
   # of those pages will not be counted
   # loop through all of those and create a new route
-  Page.where.not("slug", nil).all.each do |page|
+  Page.where("slug", nil).all.each do |page|
     get "/#{page.slug}", controller: "pages", action: "show", id: page.id
   end
   # The priority is based upon order of creation: first created -> highest priority.
