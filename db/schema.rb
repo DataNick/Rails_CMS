21aec1cc6c571555996c20b05e047053fd6e1d98
DataNick <DataNick@users.noreply.github.com>
2016-02-09 00:49:01 -0500

    Added category_id to pages schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209054053) do
+ActiveRecord::Schema.define(version: 20160209054741) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -23,8 +23,11 @@ ActiveRecord::Schema.define(version: 20160209054053) do
     t.string   "title"
     t.text     "body"
     t.string   "slug"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
+
+  add_index "pages", ["category_id"], name: "index_pages_on_category_id"
 
 end
