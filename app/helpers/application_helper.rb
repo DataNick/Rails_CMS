b7d7db1b9a481b7116dfd8848b9723bc700da61b
DataNick <DataNick@users.noreply.github.com>
2016-02-10 08:46:51 -0500

    Defined url method for menu_items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
     end
   end
 
+  def url_for_menu_item(menu_item)
+    if menu_item.url.match(/:\/\//) #http:// for example
+      menu_item.url
+    else
+      root_path + menu_item.url
+    end
+  end
+
 end
