ad26bcaaf2d2b52b326cde778fa11a06f4300f63
DataNick <DataNick@users.noreply.github.com>
2016-02-10 04:22:54 -0500

    Set up CRUD actions for menu in admin namespace

---
+++ app/controllers/admin/menus_controller.rb
@@ -0,0 +1,49 @@
+class MenusController < ApplicationController
+  before_action :set_menu, only: [:update, :edit, :destroy]
+  def index
+    @menus = Menu.all
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to admin_menus_path, notice: "Menu created"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @menu.update_attributes(menu_params)
+      redirect_to admin_menus_path, notice: "Menu updated"
+    else
+      render :edit
+    end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    if @menu.destroy
+      redirect_to admin_menus_path, notice: "Menu deleted"
+    else
+      redirect_to admin_menus_path, alert: "Menu not deleted"
+    end
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name)
+  end
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+  end
+end
