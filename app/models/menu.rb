6febc21be0f5294ba12ce16dc6b31e8367c6ac47
DataNick <DataNick@users.noreply.github.com>
2016-02-10 08:38:30 -0500

    Fixed spelling mistake

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,5 @@
 class Menu < ActiveRecord::Base
-  validates_presence_if :name
+  validates_presence_of :name
   has_many :menu_items
 
   accepts_nested_attributes_for :menu_items, reject_if: :all_blank #new method menu_items_attributes=(attributes) is added to model
