76aacdf4acc90d2814da6efdbf8329cee6495fe3
DataNick <DataNick@users.noreply.github.com>
2016-02-10 08:01:14 -0500

    Set up nested attributes method

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,5 @@
 class Menu < ActiveRecord::Base
+  has_many :menu_items
+
+  accepts_nested_attributes_for :menu_items, reject_if: :all_blank #new method menu_items_attributes=(attributes) is added to model
 end
