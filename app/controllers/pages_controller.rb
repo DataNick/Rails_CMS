713ca937c3dadd2ba89b560cd8c4ac930e4b7238
DataNick <DataNick@users.noreply.github.com>
2016-02-11 14:15:11 -0500

    Created template_to_render method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,20 @@ class PagesController < ContentController
   before_action :set_page, only: [:show, :edit, :update, :destroy]
 
   def show
-    render template: "page" #matches page haml file in default folder
+    render template: tempate_to_render #matches page haml file in default folder
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_page
-      @page = Page.find(params[:id])
-    end
+
+  protected
+
+  def template_to_render
+    "page"
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_page
+    @page = Page.find(params[:id])
+  end
 
 
 end
