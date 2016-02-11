fd9be94c0127be39583278caacfe902af768582e
DataNick <DataNick@users.noreply.github.com>
2016-02-10 23:27:14 -0500

    Set up content_controller.rb

---
+++ app/controllers/content_controller.rb
@@ -0,0 +1,9 @@
+class ContentController < ApplicationController
+  before_filter :set_theme
+
+  def set_theme
+    theme = "default"
+    prepend_view_path("app/themes/#{theme}")
+    self.class.layout("../../themes/#{theme}/layout")
+  end
+end
