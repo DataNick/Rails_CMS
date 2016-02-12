fc42390e0390b4a01570428e5cefc8c0dc11f0cc
DataNick <DataNick@users.noreply.github.com>
2016-02-11 20:27:25 -0500

    Set up index and update actions

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,7 +1,19 @@
 class Admin::SettingsController < ApplicationController
   def index
+    @settings = Setting.all
   end
 
   def update
+    setting_params.each do |key, value|
+      Setting.where(key: key).first.update_attribute(:value, value)
+    end
+    Rails.application.reload.routes!
+    redirect_to admin_settings_path, notice: "Settings saved."
+  end
+
+  private
+
+  def setting_params
+    params.require(:setting).permit(:homepage, :theme) #will have an array of settings with different keys
   end
 end
