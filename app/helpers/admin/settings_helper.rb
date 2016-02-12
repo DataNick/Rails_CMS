76deb0e94255c15d1a09367d08bc0c35416cf726
DataNick <DataNick@users.noreply.github.com>
2016-02-12 00:07:03 -0500

    Added a comma

--- app/helpers/admin/settings_helper.rb
+++ app/helpers/admin/settings_helper.rb
@@ -1,9 +1,11 @@
 module Admin::SettingsHelper
   def homepage_options
     grouped_options_for_select({
-      "Pages" => Page.all.map({|page| [page.title, url_for_page(page)]})
-      "Categories" => Category.all.map({|category| [category.name, url_for_category(category)]})
+      "Pages" => Page.all.map {|page| [page.title, url_for_page(page)]},
+      "Categories" => Category.all.map { |category| [ category.name, category_path(category) ]}
       }, Setting.where(key: "homepage").first.value)
+
+
   end
 
   def theme_options
@@ -13,3 +15,7 @@ module Admin::SettingsHelper
       )
   end
 end
+   # grouped_options_for_select({
+   #    "Pages" => Page.all.map { |page| [ page.title, url_for_page(page) ] },
+   #    "Categories" => Category.all.map { |category| [ category.name, category_path(category) ] }
+   #    }, Setting.where(key: "homepage").first.value)
